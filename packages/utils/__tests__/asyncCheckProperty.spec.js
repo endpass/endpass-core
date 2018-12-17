8ce397736bc8bee2850617055a155c13e2a172b8
Mona Lisa <Lenseg1@gmail.com>
2018-12-17 06:20:35 +0300

    utils to repo

---
+++ packages/utils/__tests__/asyncCheckProperty.spec.js
@@ -0,0 +1,20 @@
+import { asyncCheckProperty } from '../lib/asyncCheckProperty';
+
+describe('utils', () => {
+  describe('asyncCheckProperty', () => {
+    beforeAll(() => {
+      jest.useRealTimers();
+    });
+
+    it('should resolves only values matches to predicate due changing', async () => {
+      expect.assertions(1);
+
+      const foo = {
+        bar: 'beep',
+      };
+      const res = await asyncCheckProperty(foo, 'bar', v => v === 'beep', 100);
+
+      expect(res).toBe('beep');
+    });
+  });
+});
---
+++ packages/utils/lib/objects.js
@@ -0,0 +1,19 @@
+import { get } from 'lodash';
+
+export const merge = (...obj) =>
+  obj.reduce((acc, item) => Object.assign(acc, item), {});
+
+export const getFrom = (target, ...paths) => {
+  const existPath = paths.find(path => get(target, path));
+
+  if (!existPath) {
+    return null;
+  }
+
+  return get(target, existPath);
+};
+
+export default {
+  merge,
+  getFrom
+};
