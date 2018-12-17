9f90971286b327ab95ad15a1cd28a661db3af784
Mona Lisa <Lenseg1@gmail.com>
2018-12-17 06:20:35 +0300

    utils to repo

---
+++ packages/utils/lib/asyncCheckProperty.js
@@ -0,0 +1,20 @@
+import { get, identity } from 'lodash';
+
+export const asyncCheckProperty = (
+  object,
+  path,
+  predicate = identity,
+  timer = 250,
+) =>
+  new Promise(resolve => {
+    /* eslint-disable-next-line */
+    const interval = setInterval(() => {
+      const value = get(object, path);
+
+      if (predicate(value)) {
+        clearInterval(interval);
+
+        return resolve(value);
+      }
+    }, timer);
+  });
