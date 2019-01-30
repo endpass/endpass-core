a67b2a40b3df7c02734c32251c4fa7e805c5fca2
dudiq <idudiq@gmail.com>
2019-01-30 18:22:54 +0300

    feat(utils): add fromTo, addToDate methods to date utils

--- packages/utils/__tests__/date.spec.js
+++ packages/utils/__tests__/date.spec.js
@@ -1,6 +1,6 @@
-import { formateDate, fromNow } from '../date';
+import { formateDate, fromNow, fromTo, addToDate } from '../date';
 
-describe('arrays utils', () => {
+describe('date utils', () => {
   describe('formateDate', () => {
     it('should formate date by YYYY-MM-DD H:mm pattern', () => {
       expect(formateDate(new Date('2016 06 12 12:40'))).toBe(
@@ -31,6 +31,53 @@ describe('arrays utils', () => {
       expect(fromNow(testDates[2])).toBe('a day ago');
       expect(fromNow(testDates[3])).toBe('a month ago');
       expect(fromNow(testDates[4])).toBe('a year ago');
+    });
+  });
+
+  describe('fromTo', () => {
+    const now = new Date();
+    const edgeDate = new Date(0);
+    const testDates = [
+      now.setMinutes(now.getMinutes() + 1),
+      now.setHours(now.getHours() + 1),
+      now.setDate(now.getDate() + 1),
+      now.setMonth(now.getMonth() + 1),
+      now.setYear(now.getFullYear() + 1),
+      edgeDate.setMinutes(10),
+    ];
+
+
+    it('should returns date relative string', () => {
+      const checkNow = Date.now();
+      expect(fromTo(checkNow, testDates[0])).toBe('in a minute');
+      expect(fromTo(checkNow, testDates[1])).toBe('in an hour');
+      expect(fromTo(checkNow, testDates[2])).toBe('in a day');
+      expect(fromTo(checkNow, testDates[3])).toBe('in a month');
+      expect(fromTo(checkNow, testDates[4])).toBe('in a year');
+      expect(fromTo(new Date(0), testDates[5])).toBe('in 10 minutes');
+    });
+  });
+
+  describe('addToDate', () => {
+    const now = Date.now();
+
+    const sec = 1000;
+    const min = sec * 60;
+    const hour = min * 60;
+    const day = hour * 24;
+
+    const testDates = [
+      new Date(now + (sec * 10)),
+      new Date(now + (min * 4)),
+      new Date(now + (hour * 2)),
+      new Date(now + (day * 56)),
+    ];
+
+    it('should increment date', () => {
+      expect(addToDate(now, 10)).toMatchObject(testDates[0]);
+      expect(addToDate(now, 4, 'm')).toMatchObject(testDates[1]);
+      expect(addToDate(now, 2, 'h')).toMatchObject(testDates[2]);
+      expect(addToDate(now,  56, 'd')).toMatchObject(testDates[3]);
     });
   });
 });
--- packages/utils/date.js
+++ packages/utils/date.js
@@ -6,6 +6,16 @@ const relativeTime = require('dayjs/plugin/relativeTime');
  */
 dayjs.extend(relativeTime);
 
-module.exports.formateDate = date => dayjs(date).format('YYYY-MM-DD H:mm');
-
-module.exports.fromNow = date => dayjs(date).fromNow();
+module.exports = {
+  formateDate: date => dayjs(date).format('YYYY-MM-DD H:mm'),
+  fromNow: date => dayjs(date).fromNow(),
+  fromTo: (fromDate, toDate) => {
+    const start = dayjs(fromDate);
+    return start.to(dayjs(toDate));
+  },
+  addToDate: (date, value, unit = 's') => {
+    return dayjs(date)
+      .add(value, unit)
+      .toDate();
+  },
+};
