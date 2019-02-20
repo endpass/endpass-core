21e2e1553c15927c3f009c17a860db203dafeccf
idudiq <idudiq@gmail.com>
2019-02-20 16:26:47 +0300

    fix(class): revert minification

--- packages/class/dist/chunk-f6e67ce9.js
+++ packages/class/dist/chunk-f6e67ce9.js
@@ -1,1 +1,243 @@
-import{a as e,b as r,c as t,d as n}from"./chunk-350af72f.js";import{a}from"./chunk-96639b88.js";import s from"ethereumjs-tx";import c from"ethereumjs-wallet/hdkey";import{sha3 as o,toDecimal as i,toHex as u}from"web3-utils";import{e as p}from"./chunk-b4810d43.js";import{a as h}from"./chunk-25f6a05b.js";import f from"trezor-connect";import{a as d}from"./chunk-ade9ee03.js";export default(function(){function l(){n(this,l)}return e(l,null,[{key:"getNextWallets",value:function(){var e=r(t.mark(function e(r){var n,a,s,o,i,u,p,f;return t.wrap(function(e){for(;;)switch(e.prev=e.next){case 0:if(n=r.offset,a=void 0===n?0:n,s=r.limit,o=void 0===s?10:s,i=r.xpub,e.prev=1,e.t0=i,e.t0){e.next=7;break}return e.next=6,l.getPublicExtendedKey();case 6:e.t0=e.sent;case 7:return u=e.t0,p=c.fromExtendedKey(u),f=d(p,a,o),e.abrupt("return",{addresses:f,xpub:u});case 13:throw e.prev=13,e.t1=e.catch(1),new h({title:"Access error",text:"An error occurred while getting access to hardware device. Please, try again.",type:"is-danger"});case 16:case"end":return e.stop()}},e,this,[[1,13]])}));return function(r){return e.apply(this,arguments)}}()},{key:"getPublicExtendedKey",value:function(){var e=r(t.mark(function e(){var r,n,a;return t.wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.prev=0,r=ENV.hdKeyMnemonic.path,e.next=4,f.getPublicKey({path:r});case 4:return n=e.sent,a=n.payload.xpub,e.abrupt("return",a);case 9:throw e.prev=9,e.t0=e.catch(0),new h({title:"Access error",text:"An error occurred while getting access to hardware device. Please, try again.",type:"is-danger"});case 12:case"end":return e.stop()}},e,this,[[0,9]])}));return function(){return e.apply(this,arguments)}}()},{key:"sign",value:function(){var e=r(t.mark(function e(r,n){var a,s,c,h,d,l,m,v,x,g;return t.wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return a="Ethereum Signed Message:\n".concat(r.length).concat(r),s=o(a),e.next=4,f.ethereumSignMessage({path:"".concat(p).concat(n),message:r});case 4:if(c=e.sent,h=c.success,d=c.payload,h){e.next=9;break}throw new Error(d.error||"Bad Trezor response");case 9:return l=d.signature,m="0x".concat(l.slice(0,64)),v="0x".concat(l.slice(64,128)),x=i(l.slice(128,130)),g=u(27===x||28===x?x:x+27),e.abrupt("return",{message:r,messageHash:s,signature:"0x".concat(l),r:m,s:v,v:g});case 15:case"end":return e.stop()}},e,this)}));return function(r,t){return e.apply(this,arguments)}}()},{key:"signTransaction",value:function(){var e=r(t.mark(function e(r,n){var c,o,i,u;return t.wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,f.ethereumSignTransaction({path:"".concat(p).concat(n),transaction:r});case 2:if(c=e.sent,!(o=c.payload).error){e.next=6;break}throw new Error(o.error||"Bad Trezor response");case 6:return i={r:o.r,s:o.s,v:o.v},u=new s(a({},r,i)),e.abrupt("return","0x".concat(u.serialize().toString("hex")));case 9:case"end":return e.stop()}},e,this)}));return function(r,t){return e.apply(this,arguments)}}()}]),l}());
+import { a as _createClass, b as _asyncToGenerator, c as _regeneratorRuntime, d as _classCallCheck } from './chunk-350af72f.js';
+import { a as _objectSpread } from './chunk-96639b88.js';
+import Tx from 'ethereumjs-tx';
+import HDKey from 'ethereumjs-wallet/hdkey';
+import { sha3, toDecimal, toHex } from 'web3-utils';
+import { e as HARDWARE_DERIVIATION_PATH } from './chunk-b4810d43.js';
+import { a as NotificationError } from './chunk-25f6a05b.js';
+import TrezorConnect from 'trezor-connect';
+import { a as getChildrenAddress } from './chunk-ade9ee03.js';
+
+var TrezorProxy =
+/*#__PURE__*/
+function () {
+  function TrezorProxy() {
+    _classCallCheck(this, TrezorProxy);
+  }
+
+  _createClass(TrezorProxy, null, [{
+    key: "getNextWallets",
+    value: function () {
+      var _getNextWallets = _asyncToGenerator(
+      /*#__PURE__*/
+      _regeneratorRuntime.mark(function _callee(_ref) {
+        var _ref$offset, offset, _ref$limit, limit, savedXpub, xpub, hdWallet, addresses;
+
+        return _regeneratorRuntime.wrap(function _callee$(_context) {
+          while (1) {
+            switch (_context.prev = _context.next) {
+              case 0:
+                _ref$offset = _ref.offset, offset = _ref$offset === void 0 ? 0 : _ref$offset, _ref$limit = _ref.limit, limit = _ref$limit === void 0 ? 10 : _ref$limit, savedXpub = _ref.xpub;
+                _context.prev = 1;
+                _context.t0 = savedXpub;
+
+                if (_context.t0) {
+                  _context.next = 7;
+                  break;
+                }
+
+                _context.next = 6;
+                return TrezorProxy.getPublicExtendedKey();
+
+              case 6:
+                _context.t0 = _context.sent;
+
+              case 7:
+                xpub = _context.t0;
+                hdWallet = HDKey.fromExtendedKey(xpub);
+                addresses = getChildrenAddress(hdWallet, offset, limit);
+                return _context.abrupt("return", {
+                  addresses: addresses,
+                  xpub: xpub
+                });
+
+              case 13:
+                _context.prev = 13;
+                _context.t1 = _context["catch"](1);
+                throw new NotificationError({
+                  title: 'Access error',
+                  text: "An error occurred while getting access to hardware device. Please, try again.",
+                  type: 'is-danger'
+                });
+
+              case 16:
+              case "end":
+                return _context.stop();
+            }
+          }
+        }, _callee, this, [[1, 13]]);
+      }));
+
+      function getNextWallets(_x) {
+        return _getNextWallets.apply(this, arguments);
+      }
+
+      return getNextWallets;
+    }()
+  }, {
+    key: "getPublicExtendedKey",
+    value: function () {
+      var _getPublicExtendedKey = _asyncToGenerator(
+      /*#__PURE__*/
+      _regeneratorRuntime.mark(function _callee2() {
+        var path, _ref2, xpub;
+
+        return _regeneratorRuntime.wrap(function _callee2$(_context2) {
+          while (1) {
+            switch (_context2.prev = _context2.next) {
+              case 0:
+                _context2.prev = 0;
+                path = ENV.hdKeyMnemonic.path;
+                _context2.next = 4;
+                return TrezorConnect.getPublicKey({
+                  path: path
+                });
+
+              case 4:
+                _ref2 = _context2.sent;
+                xpub = _ref2.payload.xpub;
+                return _context2.abrupt("return", xpub);
+
+              case 9:
+                _context2.prev = 9;
+                _context2.t0 = _context2["catch"](0);
+                throw new NotificationError({
+                  title: 'Access error',
+                  text: "An error occurred while getting access to hardware device. Please, try again.",
+                  type: 'is-danger'
+                });
+
+              case 12:
+              case "end":
+                return _context2.stop();
+            }
+          }
+        }, _callee2, this, [[0, 9]]);
+      }));
+
+      function getPublicExtendedKey() {
+        return _getPublicExtendedKey.apply(this, arguments);
+      }
+
+      return getPublicExtendedKey;
+    }()
+  }, {
+    key: "sign",
+    value: function () {
+      var _sign = _asyncToGenerator(
+      /*#__PURE__*/
+      _regeneratorRuntime.mark(function _callee3(message, index) {
+        var prefixedMessage, messageHash, _ref3, success, payload, signature, r, s, vInt, v;
+
+        return _regeneratorRuntime.wrap(function _callee3$(_context3) {
+          while (1) {
+            switch (_context3.prev = _context3.next) {
+              case 0:
+                // \u0019 \x19
+                prefixedMessage = "\x19Ethereum Signed Message:\n".concat(message.length).concat(message);
+                messageHash = sha3(prefixedMessage);
+                _context3.next = 4;
+                return TrezorConnect.ethereumSignMessage({
+                  path: "".concat(HARDWARE_DERIVIATION_PATH).concat(index),
+                  message: message
+                });
+
+              case 4:
+                _ref3 = _context3.sent;
+                success = _ref3.success;
+                payload = _ref3.payload;
+
+                if (success) {
+                  _context3.next = 9;
+                  break;
+                }
+
+                throw new Error(payload.error || 'Bad Trezor response');
+
+              case 9:
+                signature = payload.signature;
+                r = "0x".concat(signature.slice(0, 64));
+                s = "0x".concat(signature.slice(64, 128));
+                vInt = toDecimal(signature.slice(128, 130));
+                v = vInt === 27 || vInt === 28 ? toHex(vInt) : toHex(vInt + 27);
+                return _context3.abrupt("return", {
+                  message: message,
+                  messageHash: messageHash,
+                  signature: "0x".concat(signature),
+                  r: r,
+                  s: s,
+                  v: v
+                });
+
+              case 15:
+              case "end":
+                return _context3.stop();
+            }
+          }
+        }, _callee3, this);
+      }));
+
+      function sign(_x2, _x3) {
+        return _sign.apply(this, arguments);
+      }
+
+      return sign;
+    }()
+  }, {
+    key: "signTransaction",
+    value: function () {
+      var _signTransaction = _asyncToGenerator(
+      /*#__PURE__*/
+      _regeneratorRuntime.mark(function _callee4(transaction, index) {
+        var _ref4, payload, sign, tx;
+
+        return _regeneratorRuntime.wrap(function _callee4$(_context4) {
+          while (1) {
+            switch (_context4.prev = _context4.next) {
+              case 0:
+                _context4.next = 2;
+                return TrezorConnect.ethereumSignTransaction({
+                  path: "".concat(HARDWARE_DERIVIATION_PATH).concat(index),
+                  transaction: transaction
+                });
+
+              case 2:
+                _ref4 = _context4.sent;
+                payload = _ref4.payload;
+
+                if (!payload.error) {
+                  _context4.next = 6;
+                  break;
+                }
+
+                throw new Error(payload.error || 'Bad Trezor response');
+
+              case 6:
+                sign = {
+                  r: payload.r,
+                  s: payload.s,
+                  v: payload.v
+                };
+                tx = new Tx(_objectSpread({}, transaction, sign));
+                return _context4.abrupt("return", "0x".concat(tx.serialize().toString('hex')));
+
+              case 9:
+              case "end":
+                return _context4.stop();
+            }
+          }
+        }, _callee4, this);
+      }));
+
+      function signTransaction(_x4, _x5) {
+        return _signTransaction.apply(this, arguments);
+      }
+
+      return signTransaction;
+    }()
+  }]);
+
+  return TrezorProxy;
+}();
+
+export default TrezorProxy;
