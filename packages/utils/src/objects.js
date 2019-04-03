85294323f166a43e0407ed8ecc88b27d6861cb41
Mona Lisa <Lenseg1@gmail.com>
2019-04-03 11:35:50 +0800

    feat: env parse util

--- packages/utils/src/objects.js
+++ packages/utils/src/objects.js
@@ -11,4 +11,14 @@ module.exports = {
 
     return get(target, existPath);
   },
+  parseObjectProperties: (obj, prefix) => {
+    return Object.keys(obj)
+    .filter(key => {
+      return prefix ? key.indexOf(prefix) === 0 : true;
+    })
+    .reduce((parsedObj, key) => {
+      parsedObj[key] = JSON.parse(obj[key]);
+  	  return parsedObj
+    }, {});
+  },
 };
