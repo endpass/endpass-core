2544e7919972db9ab6ce3ec61ef572f7f574ac5b
Mark <mmiller@endpass.com>
2019-04-23 16:15:18 +0300

    chore: lint rules + prettier scripts

---
+++ packages/utils/.eslintrc.js
@@ -0,0 +1,72 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  globals: {
+    Cypress: true,
+    cy: true,
+    ENV: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  plugins: ['prettier', 'json'],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx'],
+      },
+    },
+  },
+  rules: {
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    rules: {
+      'prettier/prettier': ['error'],
+    },
+    'implicit-arrow-linebreak': 'off',
+    'no-empty': 'warn',
+    'class-methods-use-this': 'off',
+    'no-underscore-dangle': 'off',
+    'max-len': [
+      'warn',
+      80,
+      2,
+      {
+        ignoreUrls: true,
+        ignoreComments: false,
+        ignoreRegExpLiterals: true,
+        ignoreStrings: false,
+        ignoreTemplateLiterals: false,
+      },
+    ],
+    'func-names': 0,
+    'no-param-reassign': [
+      'error',
+      {
+        props: true,
+        ignorePropertyModificationsFor: ['state'],
+      },
+    ],
+    'import/no-unresolved': [
+      'error',
+      {
+        ignore: ['@', 'fixtures', 'mocks'],
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.spec.js'],
+      rules: {
+        'global-require': 'off',
+      },
+    },
+  ],
+};
