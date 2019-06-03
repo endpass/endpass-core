45f851a3c057172e4cb3b600144bbb5a0c14858c
dudiq <idudiq@gmail.com>
2019-06-03 16:50:35 +0300

    refactor(ui): change view of pagination component

--- packages/ui/tests/unit/specs/components/VPagination.spec.js
+++ packages/ui/tests/unit/specs/components/VPagination.spec.js
@@ -6,7 +6,12 @@ describe('VPagination', () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallowMount(VPagination);
+    wrapper = shallowMount(VPagination, {
+      propsData: {
+        offset: 0,
+        limit: 10,
+      },
+    });
     jest.clearAllMocks();
   });
 
@@ -15,34 +20,133 @@ describe('VPagination', () => {
       expect(wrapper.name()).toBe('VPagination');
       expect(wrapper.isVueInstance()).toBeTruthy();
     });
-  });
 
-  describe('behaviour', () => {
-    it('should render only "next"', () => {
-      expect(wrapper.find('[data-test=pagination-next]').exists()).toBeTruthy();
-      expect(wrapper.find('[data-test=pagination-prev]').exists()).toBeFalsy();
+    it('should be with default state', () => {
+      wrapper.setProps({
+        limit: 10,
+        offset: 0,
+      });
+      expect(wrapper.html()).toMatchSnapshot();
     });
 
-    it('should render with "back"', () => {
+    it('should be with pagesVisible', () => {
       wrapper.setProps({
-        offset: 10,
+        limit: 10,
+        offset: 40,
+        pagesVisible: 3,
       });
+      expect(wrapper.html()).toMatchSnapshot();
+    });
 
+    it('should be with defined total', () => {
+      wrapper.setProps({
+        limit: 10,
+        offset: 50,
+        pagesVisible: 3,
+        total: 100
+      });
+      expect(wrapper.html()).toMatchSnapshot();
+    });
+
+    it('should be with defined total in end', () => {
+      wrapper.setProps({
+        limit: 10,
+        offset: 100,
+        pagesVisible: 3,
+        total: 100
+      });
+      expect(wrapper.html()).toMatchSnapshot();
+    });
+  });
+
+  describe('behaviour next/prev buttons', () => {
+    it('should render both directions', () => {
       expect(wrapper.find('[data-test=pagination-next]').exists()).toBeTruthy();
       expect(wrapper.find('[data-test=pagination-prev]').exists()).toBeTruthy();
     });
 
-    it('should render without "next"', () => {
+    it('should render with disabled "next"', () => {
       wrapper.setProps({
+        limit: 5,
         offset: 10,
+        total: 10,
+      });
+
+      expect(wrapper.find('[data-test=pagination-next]').attributes().disabled).toBeTruthy();
+      expect(wrapper.find('[data-test=pagination-prev]').attributes().disabled).toBeFalsy();
+    });
+
+    it('should render with disabled "prev"', () => {
+      wrapper.setProps({
+        offset: 0,
         limit: 10,
         total: 12,
       });
 
-      expect(wrapper.find('[data-test=pagination-next]').exists()).toBeFalsy();
-      expect(wrapper.find('[data-test=pagination-prev]').exists()).toBeTruthy();
+      expect(wrapper.find('[data-test=pagination-next]').attributes().disabled).toBeFalsy();
+      expect(wrapper.find('[data-test=pagination-prev]').attributes().disabled).toBeTruthy();
+    });
+  });
+
+  describe('first and latest buttons', () => {
+    beforeEach(() => {
+      wrapper.setProps({
+        offset: 0,
+        limit: 10,
+        total: 100,
+      });
     });
 
+    it('should not show first button', () => {
+      wrapper.setProps({
+        offset: 50,
+      });
+
+      expect(wrapper.find('[data-test=first-btn]').exists()).toBeFalsy();
+      expect(wrapper.find('[data-test=latest-btn]').exists()).toBeFalsy();
+    });
+
+    it('should not show latest button', () => {
+      wrapper.setProps({
+        offset: 0,
+      });
+
+      expect(wrapper.find('[data-test=first-btn]').exists()).toBeFalsy();
+      expect(wrapper.find('[data-test=latest-btn]').exists()).toBeFalsy();
+    });
+
+    it('should show only latest button', () => {
+      wrapper.setProps({
+        offset: 0,
+        pagesVisible: 3,
+      });
+
+      expect(wrapper.find('[data-test=first-btn]').exists()).toBeFalsy();
+      expect(wrapper.find('[data-test=latest-btn]').exists()).toBeTruthy();
+    });
+
+    it('should show first and latest button', () => {
+      wrapper.setProps({
+        offset: 50,
+        pagesVisible: 3,
+      });
+
+      expect(wrapper.find('[data-test=first-btn]').exists()).toBeTruthy();
+      expect(wrapper.find('[data-test=latest-btn]').exists()).toBeTruthy();
+    });
+
+    it('should show only first button', () => {
+      wrapper.setProps({
+        offset: 90,
+        pagesVisible: 3,
+      });
+
+      expect(wrapper.find('[data-test=first-btn]').exists()).toBeTruthy();
+      expect(wrapper.find('[data-test=latest-btn]').exists()).toBeFalsy();
+    });
+  });
+
+  describe('events', () => {
     it('should emit correct next offset', () => {
       wrapper.setProps({
         offset: 0,
