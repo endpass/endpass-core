07c601ba5efa1860e4de4e7b3da9b0bd9a59b584
dudiq <idudiq@gmail.com>
2019-06-07 19:38:04 +0300

    fix: declare theme provider

--- packages/ui/src/components/index.js
+++ packages/ui/src/components/index.js
@@ -13,3 +13,4 @@ export { default as VSpinner } from '@/components/VSpinner';
 export { default as VTextarea } from '@/components/VTextarea';
 export { default as VSvgIcon } from '@/components/VSvgIcon';
 export { default as VBadge } from '@/components/VBadge';
+export { default as ThemeProvider } from '@/components/ThemeProvider';
