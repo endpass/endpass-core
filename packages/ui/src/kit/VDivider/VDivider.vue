367d3297439d57eef914fcdea518a02f5db7aca5
Mark <mmiller@endpass.com>
2019-06-17 10:58:56 +0300

    feat(ui): add VDivider component

---
+++ packages/ui/src/kit/VDivider/VDivider.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="v-divider"
+    :class="themeCssClass"
+  >
+    <hr class="v-divider-before">
+    <span
+      v-if="$slots.default"
+      class="v-divider-text"
+    >
+      <slot />
+    </span>
+    <hr class="v-divider-after">
+  </div>
+</template>
+
+<script>
+import ThemeMixin from '@/mixins/ThemeMixin';
+
+export default {
+  name: 'VDivider',
+  mixins: [ThemeMixin],
+};
+</script>
