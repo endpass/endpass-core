ab3d44720e8bf668a734dca82569ef4b41db44e0
Mark Miller <mmiller@endpass.com>
2019-07-19 13:05:16 +0300

    feat(ui): add v-card component (#169)

---
+++ packages/ui/src/kit/VCard/VCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    class="v-card"
+    :class="themeCssClass"
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+import ThemeMixin from '@/mixins/ThemeMixin';
+
+export default {
+  name: 'VCard',
+  mixins: [ThemeMixin],
+};
+</script>
