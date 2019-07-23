fb20502eb5f258332ad565d9e23bdeb18e8e1814
dudiq <idudiq@gmail.com>
2019-07-23 11:41:24 +0300

    feat(ui): scroll in side-bar

---
+++ packages/ui/src/kit/VSidebarContainer/index.js
@@ -0,0 +1,1 @@
+export { default } from './VSidebarContainer.vue';
--- packages/ui/stories/VSideBar.stories.js
+++ packages/ui/stories/VSideBar.stories.js
@@ -9,9 +9,10 @@ import VSvgIcon from '@/kit/VSvgIcon';
 import VSidebar from '@/kit/VSidebar';
 import VSidebarHeader from '@/kit/VSidebarHeader';
 import VSidebarPanel from '@/kit/VSidebarPanel';
+import VSidebarContainer from '@/kit/VSidebarContainer';
 
 storiesOf('VSidebar/desktop', module)
-  .add('left', () => ({
+  .add('left (fixed header)', () => ({
     data: () => ({
       isSidebarVisible: false,
     }),
@@ -40,6 +41,7 @@ storiesOf('VSidebar/desktop', module)
       VSidebar,
       VSidebarHeader,
       VSidebarPanel,
+      VSidebarContainer,
     },
     template: `
       <theme-provider>
@@ -59,17 +61,19 @@ storiesOf('VSidebar/desktop', module)
                 <span slot="title">Title</span>
                 Subtitle
               </v-sidebar-header>
-              <v-divider/>
-              <v-sidebar-panel>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
-              </v-sidebar-panel>
+              <v-sidebar-container>
+                <v-sidebar-panel>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.
+                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? 
+                </v-sidebar-panel>
+              </v-sidebar-container>
             </v-sidebar>
           </div>
         </div>
       </theme-provider>
     `,
   }))
-  .add('right', () => ({
+  .add('right (full scrollable)', () => ({
     data: () => ({
       isSidebarVisible: false,
     }),
@@ -119,7 +123,8 @@ storiesOf('VSidebar/desktop', module)
               </v-sidebar-header>
               <v-divider/>
               <v-sidebar-panel>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? 
               </v-sidebar-panel>
             </v-sidebar>
           </div>
