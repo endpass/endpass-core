d6ca384437591e2073f07f06b20a9635b125ab74
lamartire <lamartire@gmail.com>
2019-08-20 11:28:17 +0300

    feat(eslint-plugin): add extra rules for some cases

--- packages/eslint-plugin/lib/configs/essential.js
+++ packages/eslint-plugin/lib/configs/essential.js
@@ -1,35 +1,45 @@
-const base = require('./base');
+const base = require("./base");
 
 module.exports = {
   ...base,
   rules: {
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'prettier/prettier': ['error'],
-    'arrow-parens': ['error', 'as-needed'],
-    'no-shadow': ['warn'],
-    'consistent-return': 'error',
-    'operator-linebreak': 'off',
-    'space-before-function-paren': 'off',
-    'function-paren-newline': 'off',
-    'object-curly-newline': 'off',
-
-    'implicit-arrow-linebreak': 'off',
-    'no-empty': 'warn',
-    'class-methods-use-this': 'off',
-    'no-underscore-dangle': 'off',
-    'func-names': 0,
-    'no-param-reassign': [
-      'error',
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-console": [
+      "error",
+      {
+        allow: ["warn", "error"]
+      }
+    ],
+    "no-shadow": ["warn"],
+    "no-empty": [
+      "warn",
+      {
+        allowEmptyCatch: true
+      }
+    ],
+    "no-underscore-dangle": "off",
+    "no-param-reassign": [
+      "error",
       {
         props: true,
-        ignorePropertyModificationsFor: ['state'],
-      },
+        ignorePropertyModificationsFor: ["state"]
+      }
     ],
-    'import/no-unresolved': [
-      'error',
+    "prettier/prettier": ["error"],
+    "arrow-parens": ["error", "as-needed"],
+    "consistent-return": "error",
+    "operator-linebreak": "off",
+    "space-before-function-paren": "off",
+    "function-paren-newline": "off",
+    "object-curly-newline": "off",
+    "implicit-arrow-linebreak": "off",
+    "class-methods-use-this": "off",
+    "func-names": 0,
+    "import/no-unresolved": [
+      "error",
       {
-        ignore: ['@', 'fixtures', 'mocks'],
-      },
-    ],
-  },
+        ignore: ["@", "fixtures", "mocks"]
+      }
+    ]
+  }
 };
