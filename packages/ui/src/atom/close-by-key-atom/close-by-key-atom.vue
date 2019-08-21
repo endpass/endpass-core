609d5da15b8d84dac0202d114208cd5e4f6577c7
dudiq <idudiq@gmail.com>
2019-08-21 11:10:37 +0300

    feat(ui): add close by escape keypress

---
+++ packages/ui/src/atom/close-by-key-atom/close-by-key-atom.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: 'CloseByKeyAtom',
+  methods: {
+    onKeyUp(e) {
+      if (e.keyCode === 27 || e.key === 'Escape') {
+        this.$emit('close', e);
+      }
+    },
+  },
+  beforeMount() {
+    window.addEventListener('keyup', this.onKeyUp);
+  },
+  beforeDestroy() {
+    window.removeEventListener('keyup', this.onKeyUp);
+  },
+  render(createElement) {
+    return createElement('div', this.$slots.default);
+  },
+};
+</script>
