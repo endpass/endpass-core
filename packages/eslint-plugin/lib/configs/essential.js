7342f55cdf32e461a001d0bf321be19f9c059f90
lamartire <lamartire@gmail.com>
2019-08-22 12:35:03 +0300

    fix(eslint-plugin): prettier rule

--- packages/eslint-plugin/lib/configs/essential.js
+++ packages/eslint-plugin/lib/configs/essential.js
@@ -1,45 +1,59 @@
-const base = require("./base");
+const base = require('./base');
 
 module.exports = {
   ...base,
   rules: {
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-console": [
-      "error",
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': [
+      'error',
       {
-        allow: ["warn", "error"]
-      }
+        allow: ['warn', 'error'],
+      },
     ],
-    "no-shadow": ["warn"],
-    "no-empty": [
-      "warn",
+    'no-shadow': ['warn'],
+    'no-empty': [
+      'warn',
       {
-        allowEmptyCatch: true
-      }
+        allowEmptyCatch: true,
+      },
     ],
-    "no-underscore-dangle": "off",
-    "no-param-reassign": [
-      "error",
+    'no-underscore-dangle': 'off',
+    'no-param-reassign': [
+      'error',
       {
         props: true,
-        ignorePropertyModificationsFor: ["state"]
-      }
+        ignorePropertyModificationsFor: ['state'],
+      },
     ],
-    "prettier/prettier": ["error"],
-    "arrow-parens": ["error", "as-needed"],
-    "consistent-return": "error",
-    "operator-linebreak": "off",
-    "space-before-function-paren": "off",
-    "function-paren-newline": "off",
-    "object-curly-newline": "off",
-    "implicit-arrow-linebreak": "off",
-    "class-methods-use-this": "off",
-    "func-names": 0,
-    "import/no-unresolved": [
-      "error",
+    'prettier/prettier': [
+      'error',
       {
-        ignore: ["@", "fixtures", "mocks"]
-      }
-    ]
-  }
+        printWidth: 80,
+        tabWidth: 2,
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+        trailingComma: 'all',
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+        requirePragma: false,
+        insertPragma: false,
+      },
+    ],
+    'arrow-parens': ['error', 'as-needed'],
+    'consistent-return': 'error',
+    'operator-linebreak': 'off',
+    'space-before-function-paren': 'off',
+    'function-paren-newline': 'off',
+    'object-curly-newline': 'off',
+    'implicit-arrow-linebreak': 'off',
+    'class-methods-use-this': 'off',
+    'func-names': 0,
+    'import/no-unresolved': [
+      'error',
+      {
+        ignore: ['@', 'fixtures', 'mocks'],
+      },
+    ],
+  },
 };
