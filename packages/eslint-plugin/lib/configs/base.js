a7877e81fdc2c5d7b5d12625ffbbd3d50b755a3e
idudiq <idudiq@gmail.com>
2019-08-29 15:48:17 +0300

    feat(eslint-plugin): add typescript support

--- packages/eslint-plugin/lib/configs/base.js
+++ packages/eslint-plugin/lib/configs/base.js
@@ -12,9 +12,9 @@ module.exports = {
     ENV: true,
   },
   extends: ['airbnb-base'],
-  plugins: ['prettier', 'json'],
+  plugins: ['prettier', 'json', '@typescript-eslint'],
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: 'babel-eslint'
   },
   overrides: [
     {
@@ -23,5 +23,20 @@ module.exports = {
         'global-require': 'off',
       },
     },
+    {
+      files: ['*.ts'],
+      plugins: ['@typescript-eslint'],
+      parserOptions: {
+        parser: '@typescript-eslint/parser'
+      },
+      rules: {
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': ['error', {
+          vars: 'all',
+          args: 'after-used',
+          ignoreRestSiblings: false
+        }]
+      }
+    }
   ],
 };
