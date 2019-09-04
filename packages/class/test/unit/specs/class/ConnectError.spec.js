828b6ae29055082de952b1b1a0dd5dd4058cfcba
idudiq <idudiq@gmail.com>
2019-09-04 11:23:00 +0300

    feat(class): improve connectError

--- packages/class/test/unit/specs/class/ConnectError.spec.js
+++ packages/class/test/unit/specs/class/ConnectError.spec.js
@@ -24,4 +24,44 @@ describe('ConnectError', () => {
     expect(err).toEqual(checkError);
     expect(err.code).toBe(ERRORS.AUTH_CANCELED_BY_USER);
   });
+
+  it('should pass already created error without code', () => {
+    const customMessage = 'my custom msg';
+    const checkError = new Error(customMessage);
+
+    const err = ConnectError.createFromError(
+      checkError,
+      ERRORS.AUTH_CANCELED_BY_USER,
+    );
+
+    expect(err).toEqual(checkError);
+    expect(err.code).toBe(ERRORS.AUTH_CANCELED_BY_USER);
+  });
+
+  it('should pass already created error with code', () => {
+    const customMessage = 'my custom msg';
+    const checkError = new Error(customMessage);
+    checkError.code = ERRORS.REQUEST_DATA;
+
+    const err = ConnectError.createFromError(
+      checkError,
+      ERRORS.AUTH_CANCELED_BY_USER,
+    );
+
+    expect(err).toBe(checkError);
+    expect(err.code).toBe(ERRORS.REQUEST_DATA);
+  });
+
+  it('should pass error as text', () => {
+    const customMessage = 'my custom msg';
+    const checkError = ConnectError.create(ERRORS.AUTH_CANCELED_BY_USER);
+
+    const err = ConnectError.createFromError(
+      customMessage,
+      ERRORS.AUTH_CANCELED_BY_USER,
+    );
+
+    expect(err).toEqual(checkError);
+    expect(err.code).toBe(ERRORS.AUTH_CANCELED_BY_USER);
+  });
 });
