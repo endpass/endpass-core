33bfc0bd5d42288b593d8629cdc6826f4cf1e24c
Mark <mmiller@endpass.com>
2019-09-09 17:59:04 +0300

    feat(ui): v-toggle

---
+++ packages/ui/src/atom/toggle-atom/toggle-atom.vue
@@ -0,0 +1,34 @@
+<template>
+  <label
+    class="toggle-atom"
+    :class="toggleAtomCssClass"
+  >
+    <slot />
+    <span class="toggle-atom-handle" />
+  </label>
+</template>
+
+<script>
+import ThemeMixin from '@/mixins/ThemeMixin';
+
+export default {
+  name: 'ToggleAtom',
+  props: {
+    size: {
+      type: String,
+      required: true,
+      validator(value) {
+        return ['normal', 'mini'].indexOf(value) !== -1;
+      },
+    },
+  },
+  computed: {
+    toggleAtomCssClass() {
+      return Object.assign({}, this.themeCssClass, {
+        [`size-${this.size}`]: true,
+      });
+    },
+  },
+  mixins: [ThemeMixin],
+};
+</script>
---
+++ packages/ui/stories/VToggle.stories.js
@@ -0,0 +1,194 @@
+import { storiesOf } from '@storybook/vue';
+import { action } from '@storybook/addon-actions';
+import VToggle from '@/kit/VToggle';
+
+const methods = {
+  onChange: action('onChange'),
+};
+
+storiesOf('VToggle/desktop', module)
+  .add('default state false', () => ({
+    methods,
+    components: { VToggle },
+    data() {
+      return {
+        value: false,
+        valueMini: false,
+      };
+    },
+    template: `
+      <theme-provider>
+        <v-toggle
+          v-model="value"
+          @change="onChange"
+        >
+          state: {{ value }}
+        </v-toggle>
+        <br/>
+        <v-toggle
+          size="mini"
+          v-model="valueMini"
+          @change="onChange"
+        >
+          state: {{ valueMini }}
+        </v-toggle>
+      </theme-provider>
+    `,
+  }))
+
+  .add('default state true', () => ({
+    methods,
+    components: { VToggle },
+    data() {
+      return {
+        value: true,
+        valueMini: true,
+      };
+    },
+    template: `
+      <theme-provider>
+        <v-toggle
+          v-model="value"
+          @change="onChange"
+        >
+          state: {{ value }}
+        </v-toggle>
+        <br/>
+        <v-toggle
+          size="mini"
+          v-model="valueMini"
+          @change="onChange"
+        >
+          state: {{ valueMini }}
+        </v-toggle>
+      </theme-provider>
+    `,
+  }))
+
+  .add('default state', () => ({
+    methods,
+    components: { VToggle },
+    data() {
+      return {
+        model: false,
+        modelMini: false,
+      };
+    },
+    template: `
+      <theme-provider>
+        <v-toggle
+          v-model="model"
+          name="Name"
+          @change="onChange"
+        >
+          state: {{ model }}
+        </v-toggle>
+        <br/>
+        <v-toggle
+          size="mini"
+          name="Name"
+          v-model="modelMini"
+          @change="onChange"
+        >
+          state: {{ modelMini }}
+        </v-toggle>
+      </theme-provider>
+    `,
+  }))
+  .add('array', () => ({
+    methods,
+    components: { VToggle },
+    data() {
+      return {
+        model: ['Bob', 'Alice'],
+        modelMini: ['Bob', 'Alice'],
+      };
+    },
+    template: `
+      <theme-provider>
+        <v-toggle
+          v-model="model"
+          value="Crypto!"
+          name="Name"
+          @change="onChange"
+        >
+          with Crypto!
+        </v-toggle>
+        <div>persons: {{ model.join(', ') }}</div>
+        <v-toggle
+          size="mini"
+          name="Name"
+          value="Crypto!"
+          v-model="modelMini"
+          @change="onChange"
+        >
+          with Crypto!
+        </v-toggle>
+        <div>persons: {{ modelMini.join(', ') }}</div>
+      </theme-provider>
+    `,
+  }))
+  .add('true/false attr', () => ({
+    methods,
+    components: { VToggle },
+    data() {
+      return {
+        model: 'no',
+        modelMini: 'no',
+      };
+    },
+    template: `
+      <theme-provider>
+        <v-toggle
+          v-model="model"
+          name="Name"
+          true-value="yes"
+          false-value="no"
+          @change="onChange"
+        >
+          state: {{ model }}
+        </v-toggle>
+        <br/>
+        <v-toggle
+          size="mini"
+          name="Name"
+          true-value="yes"
+          false-value="no"
+          v-model="modelMini"
+          @change="onChange"
+        >
+          state: {{ modelMini }}
+        </v-toggle>
+      </theme-provider>
+    `,
+  }))
+  .add('disabled', () => ({
+    methods,
+    components: { VToggle },
+    data() {
+      return {
+        model: null,
+      };
+    },
+    template: `
+      <theme-provider>
+        <v-toggle
+          v-model="model"
+          disabled
+          @input="onChange"
+        >
+          disabled
+        </v-toggle>
+        <br/>
+        <v-toggle
+          v-model="model"
+          disabled
+          size="mini"
+          :true-value="null"
+          @input="onChange"
+        >
+          disabled
+        </v-toggle>
+      </theme-provider>
+    `,
+  }));
