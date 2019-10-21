67c79f3236bc4db9d684055b5d8f0b96b350f6ec
Danil Kosterin <raymondcostner@gmail.com>
2019-10-21 10:01:13 +0300

    fix(utils): describe types for remaining functions of `date`

--- packages/utils/types/date.d.ts
+++ packages/utils/types/date.d.ts
@@ -1,3 +1,6 @@
 declare module '@endpass/utils/date' {
   function formateDate(date: Date): string;
+  function fromNow(date: Date): string;
+  function fromTo(fromDate: Date, toDate: Date): string;
+  function addToDate(date: Date, value: number, unit: string): Date;
 }
