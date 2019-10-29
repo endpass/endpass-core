68ec52748ce8c99dec984af396a6a8efb4d40b52
idudiq <idudiq@gmail.com>
2019-10-29 11:14:43 +0300

    refactor(class): remove ConnectError
    BREAKING CHANGE: remove ConnectError

--- packages/class/src/index.js
+++ packages/class/src/index.js
@@ -23,4 +23,3 @@ export { default as Token } from './Token';
 export { default as CrossWindowMessenger } from './CrossWindowMessenger';
 export { default as QueueArray } from './QueueArray';
 export { default as QueueAsync } from './QueueAsync';
-export { default as ConnectError } from './ConnectError';
