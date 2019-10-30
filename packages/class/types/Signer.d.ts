908fada03fe8ec0ecfca16922ddaeecc02a2969b
idudiq <idudiq@gmail.com>
2019-10-30 15:17:11 +0300

    fix(class): declare body as optional parameter in Request

--- packages/class/types/Signer.d.ts
+++ packages/class/types/Signer.d.ts
@@ -1,11 +1,11 @@
 type SignData = {
-  message: string,
-  messageHash: string,
-  v: string,
-  r: string,
-  s: string,
-  signature: string,
-}
+  message: string;
+  messageHash: string;
+  v: string;
+  r: string;
+  s: string;
+  signature: string;
+};
 
 declare class Signer {
   static sign(data: string, privateKey: string | Buffer): SignData;
