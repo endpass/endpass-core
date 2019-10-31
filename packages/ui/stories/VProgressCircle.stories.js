a6e65f6f60f961e131d8be269aeba2a2e200117c
idudiq <idudiq@gmail.com>
2019-10-31 12:51:28 +0300

    refactor(ui): update deps, change circle progress view logic

--- packages/ui/stories/VProgressCircle.stories.js
+++ packages/ui/stories/VProgressCircle.stories.js
@@ -1,50 +1,64 @@
 import { storiesOf } from '@storybook/vue';
 import VProgressCircle from '@/kit/VProgressCircle';
 
-storiesOf('VProgressCircle/desktop', module)
-  .add('default', () => ({
-    components: { VProgressCircle },
-    template: `
+storiesOf('VProgressCircle/desktop', module).add('default', () => ({
+  components: { VProgressCircle },
+  template: `
       <theme-provider>
         <table class="storybook-table">
-          <tr>
-            <td>
-              <v-progress-circle progress="50"/>
-            </td>
-            <td>
-              <v-progress-circle progress="75"/>
-            </td>
-            <td>
-              <v-progress-circle progress="5"/>
-            </td>
-            <td>
-              <v-progress-circle progress="95"/>
-            </td>
-          </tr>
+          <thead>
+            <tr>
+              <th>0</th>
+              <th>50</th>
+              <th>75</th>
+              <th>5</th>
+              <th>95</th>
+              <th>100</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <v-progress-circle progress="0"/>
+              </td>
+              <td>
+                <v-progress-circle progress="50"/>
+              </td>
+              <td>
+                <v-progress-circle progress="75"/>
+              </td>
+              <td>
+                <v-progress-circle progress="5"/>
+              </td>
+              <td>
+                <v-progress-circle progress="95"/>
+              </td>
+              <td>
+                <v-progress-circle progress="100"/>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <v-progress-circle progress="0" :is-label-visible="true" />
+              </td>
+              <td>
+                <v-progress-circle progress="50" :is-label-visible="true" />
+              </td>
+              <td>
+                <v-progress-circle progress="75" :is-label-visible="true" />
+              </td>
+              <td>
+                <v-progress-circle progress="5" :is-label-visible="true" />
+              </td>
+              <td>
+                <v-progress-circle progress="95" :is-label-visible="true" />
+              </td>
+              <td>
+                <v-progress-circle progress="100" :is-label-visible="true" />
+              </td>
+            </tr>
+          </tbody>
         </table>
       </theme-provider>
     `,
-  }))
-  .add('with label', () => ({
-    components: { VProgressCircle },
-    template: `
-      <theme-provider>
-        <table class="storybook-table">
-          <tr>
-            <td>
-              <v-progress-circle progress="50" :is-label-visible="true" />
-            </td>
-            <td>
-              <v-progress-circle progress="75" :is-label-visible="true" />
-            </td>
-            <td>
-              <v-progress-circle progress="5" :is-label-visible="true" />
-            </td>
-            <td>
-              <v-progress-circle progress="95" :is-label-visible="true" />
-            </td>
-          </tr>
-        </table>
-      </theme-provider>
-    `,
-  }));
+}));
