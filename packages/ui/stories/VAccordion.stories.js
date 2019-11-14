600d09d06eb864546accfe5e29da4ec57e924b9a
Danil Kosterin <raymondcostner@gmail.com>
2019-11-14 12:56:40 +0300

    fix(ui): expand accordion story

--- packages/ui/stories/VAccordion.stories.js
+++ packages/ui/stories/VAccordion.stories.js
@@ -1,12 +1,37 @@
 import { storiesOf } from '@storybook/vue';
 import VAccordion from '@/kit/VAccordion';
+import VAccordionItem from '@/kit/VAccordion/VAccordionItem';
 
 storiesOf('VAccordion/desktop', module)
   .add('default', () => ({
-    components: { VAccordion },
+    components: { VAccordion, VAccordionItem },
     template: `
       <theme-provider>
-        <v-accordion />
+        <v-accordion>
+          <v-accordion-item>
+            <template v-slot:title>Item #1</template>
+
+            <template v-slot:content>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </template>
+          </v-accordion-item>
+
+          <v-accordion-item>
+            <template v-slot:title>Item #2</template>
+
+            <template v-slot:content>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </template>
+          </v-accordion-item>
+
+          <v-accordion-item>
+            <template v-slot:title>Item #3</template>
+
+            <template v-slot:content>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </template>
+          </v-accordion-item>
+        </v-accordion>
       </theme-provider>
     `,
   }));
