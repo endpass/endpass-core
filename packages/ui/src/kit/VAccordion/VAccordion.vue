4c72b5a244336f8bf1393309435a36f82ae37a41
Danil Kosterin <raymondcostner@gmail.com>
2019-11-14 15:37:08 +0300

    fix(ui): add additional cases into a story

--- packages/ui/src/kit/VAccordion/VAccordion.vue
+++ packages/ui/src/kit/VAccordion/VAccordion.vue
@@ -14,7 +14,7 @@ export default {
   props: {
     className: {
       type: String,
-      default: '',
+      default: null,
     },
   },
   computed: {
--- packages/ui/stories/VAccordion.stories.js
+++ packages/ui/stories/VAccordion.stories.js
@@ -7,29 +7,43 @@ storiesOf('VAccordion/desktop', module)
     components: { VAccordion, VAccordionItem },
     template: `
       <theme-provider>
+        <v-accordion :className="custom-accordion-class">
+          <v-accordion-item :className="custom-item-class">
+            <template v-slot:title>Item #1</template>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          </v-accordion-item>
+          <v-accordion-item :className="custom-item-class">
+            <template v-slot:title>Item #2</template>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </v-accordion-item>
+          <v-accordion-item :className="custom-item-class">
+            <template v-slot:title>Item #3</template>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </v-accordion-item>
+        </v-accordion>
+
         <v-accordion>
           <v-accordion-item>
             <template v-slot:title>Item #1</template>
-
-            <template v-slot:content>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            </template>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
           </v-accordion-item>
-
           <v-accordion-item>
             <template v-slot:title>Item #2</template>
-
-            <template v-slot:content>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            </template>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </v-accordion-item>
-
           <v-accordion-item>
             <template v-slot:title>Item #3</template>
-
-            <template v-slot:content>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            </template>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
           </v-accordion-item>
         </v-accordion>
       </theme-provider>
