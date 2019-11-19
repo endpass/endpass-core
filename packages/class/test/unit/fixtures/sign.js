d37fb8d3139491104fb0e03713966fe5ed79e404
idudiq <idudiq@gmail.com>
2019-11-19 12:03:03 +0300

    feat(class): add recover logic to Signer

--- packages/class/test/unit/fixtures/sign.js
+++ packages/class/test/unit/fixtures/sign.js
@@ -1,12 +1,16 @@
 export const message = 'message';
 
+export const signAddress = '0x31ea8795EE32D782C8ff41a5C68Dcbf0F5B27f6d';
+
+export const signature =
+  '0x0c3c4d7e3f0966028cbe45bc09288c4738286f335583cab472a1cc3017d6bf4122df4c555edc94ca323f5d9a5601cc7e5d7d68f781770453f1a0079f9644dbb81c';
+
 export const signResult = {
   message: 'message',
   messageHash:
     '0x7f6c0e5c497ded52462ec18daeb1c94cefa11cd6949ebdb7074b2a32cac13fba',
   r: '0x0c3c4d7e3f0966028cbe45bc09288c4738286f335583cab472a1cc3017d6bf41',
   s: '0x22df4c555edc94ca323f5d9a5601cc7e5d7d68f781770453f1a0079f9644dbb8',
-  signature:
-    '0x0c3c4d7e3f0966028cbe45bc09288c4738286f335583cab472a1cc3017d6bf4122df4c555edc94ca323f5d9a5601cc7e5d7d68f781770453f1a0079f9644dbb81c',
+  signature,
   v: '0x1c',
 };
--- packages/class/types/external.d.ts
+++ packages/class/types/external.d.ts
@@ -4,5 +4,10 @@ declare module 'eth-lib/lib/hash' {
 
 declare module 'eth-lib/lib/account' {
   function sign(messageHash: string, privateKey: string): string;
+
   function decodeSignature(data: string): string[];
+
+  function recover(hash: string, signature: string): string;
+
+  function encodeSignature(signature: string[]): string;
 }
