00596edafb88bdcbe45a84c72efccfcc9819ed1e
idudiq <idudiq@gmail.com>
2019-11-26 16:41:31 +0300

    feat(ui): improve v-tabs for pass is-active prop and click action

--- packages/ui/stories/VTabs.stories.js
+++ packages/ui/stories/VTabs.stories.js
@@ -1,18 +1,24 @@
 import { storiesOf } from '@storybook/vue';
+import { action } from '@storybook/addon-actions';
 import VTabs from '@/kit/VTabs';
 import VTab from '@/kit/VTab';
 
+const methods = {
+  onClick: action('onClick'),
+};
+
 storiesOf('VTabs/desktop', module).add('default', () => ({
+  methods,
   components: { VTabs, VTab },
   template: `
       <theme-provider>
         <v-tabs>
-          <v-tab label="Foo" data-test="foo-tab">
+            <v-tab label="Foo" data-test="foo-tab" @click="onClick">
             Foo content
           </v-tab>
-          <v-tab label="Bar" data-test="bar-tab">
+          <v-tab label="Bar" data-test="bar-tab" @click="onClick">
             Bar content
-          </v-tab>
+          </v-tab>          
         </v-tabs>
       </theme-provider>
     `,
