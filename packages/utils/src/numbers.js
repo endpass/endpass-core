271a0d27e9c1bccf1b7e97166d626c626b61f766
lamartire <lamartire@gmail.com>
2019-12-11 13:07:28 +0300

    feat(utils): add week and amount utils

--- packages/utils/src/numbers.js
+++ packages/utils/src/numbers.js
@@ -1,2 +1,8 @@
-// eslint-disable-next-line
-module.exports.isNumeric = n => !isNaN(parseFloat(n)) && isFinite(n);
+const isNumeric = n => !isNaN(parseFloat(n)) && isFinite(n);
+
+const toFixed = num => (!num ? '0' : (num / 100).toFixed(2));
+
+module.exports = {
+  isNumeric,
+  toFixed,
+};
