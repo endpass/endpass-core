b9cfd4c4f909ed6581c757bbbd3bb4177e6bef69
lamartire <lamartire@gmail.com>
2019-12-11 13:07:28 +0300

    feat(utils): add week and amount utils

--- packages/utils/src/date.js
+++ packages/utils/src/date.js
@@ -1,21 +1,34 @@
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import weekOfYear from 'dayjs/plugin/weekOfYear';
 
 /**
  * Setting up dayjs globally
  */
 dayjs.extend(relativeTime);
+dayjs.extend(weekOfYear);
+
+const formateDate = (date, template = 'YYYY-MM-DD H:mm') =>
+  dayjs(date).format(template);
+
+const fromNow = date => dayjs(date).fromNow();
+
+const fromTo = (fromDate, toDate) => {
+  const start = dayjs(fromDate);
+  return start.to(dayjs(toDate));
+};
+
+const addToDate = (date, value, unit = 's') =>
+  dayjs(date)
+    .add(value, unit)
+    .toDate();
+
+const getWeek = date => dayjs(date).week();
 
 module.exports = {
-  formateDate: (date, template = 'YYYY-MM-DD H:mm') =>
-    dayjs(date).format(template),
-  fromNow: date => dayjs(date).fromNow(),
-  fromTo: (fromDate, toDate) => {
-    const start = dayjs(fromDate);
-    return start.to(dayjs(toDate));
-  },
-  addToDate: (date, value, unit = 's') =>
-    dayjs(date)
-      .add(value, unit)
-      .toDate(),
+  formateDate,
+  fromNow,
+  fromTo,
+  addToDate,
+  getWeek,
 };
