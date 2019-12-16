c5e74476258bcfa691e140d75aa2901522fc1823
Danil Kosterin <raymondcostner@gmail.com>
2019-12-16 18:19:11 +0300

    feat(ui): create `VError` component for displaying short error messages inline

---
+++ packages/ui/src/kit/VError/VError.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="v-error"
+    :class="themeCssClass"
+  >
+    <error-atom
+      v-bind="$attrs"
+    />
+  </div>
+</template>
+
+<script>
+import ThemeMixin from '@/mixins/ThemeMixin';
+import ErrorAtom from '@/atom/error-atom/error-atom';
+
+export default {
+  name: 'VError',
+
+  mixins: [ThemeMixin],
+
+  components: {
+    ErrorAtom,
+  },
+};
+</script>
