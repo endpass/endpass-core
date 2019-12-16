e8b5c75ad4502eccaf32c937979eef697c1706d0
Danil Kosterin <raymondcostner@gmail.com>
2019-12-16 19:25:45 +0300

    fix(ui): now `VError` extending atom directly

--- packages/ui/src/kit/VError/VError.vue
+++ packages/ui/src/kit/VError/VError.vue
@@ -1,25 +1,9 @@
-<template>
-  <div
-    class="v-error"
-    :class="themeCssClass"
-  >
-    <error-atom
-      v-bind="$attrs"
-    />
-  </div>
-</template>
-
 <script>
-import ThemeMixin from '@/mixins/ThemeMixin';
 import ErrorAtom from '@/atom/error-atom/error-atom';
 
 export default {
   name: 'VError',
 
-  mixins: [ThemeMixin],
-
-  components: {
-    ErrorAtom,
-  },
+  extends: ErrorAtom,
 };
 </script>
