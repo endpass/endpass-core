63ada1ef195bf71875ad531c2a797cf988e76589
idudiq <idudiq@gmail.com>
2019-12-23 14:23:05 +0300

    feat(utils): add utc date helpers

--- packages/utils/src/date.js
+++ packages/utils/src/date.js
@@ -1,6 +1,8 @@
+const get = require('lodash/get');
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import weekOfYear from 'dayjs/plugin/weekOfYear';
+
 
 /**
  * Setting up dayjs globally
@@ -25,10 +27,52 @@ const addToDate = (date, value, unit = 's') =>
 
 const getWeek = date => dayjs(date).week();
 
+const toEqualLocalTime = value => {
+  const date = new Date(value);
+  const userTimezoneOffset = date.getTimezoneOffset() * 60000;
+  const normalizedDate = new Date(date.getTime() + userTimezoneOffset);
+  return normalizedDate;
+};
+
+const dayBeginInUTC = value => {
+  const date = new Date(value);
+
+  const utcDate = Date.UTC(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
+    0,
+    0,
+    0,
+  );
+
+  return utcDate;
+};
+
+const localDateFromApi = (rawObject, fieldName) => {
+  const dateValue = get(rawObject, fieldName, null);
+  const normalizedDate = dateValue ? toEqualLocalTime(dateValue * 1000) : null;
+
+  return normalizedDate;
+};
+
+const localDateToApi = (rawObject, fieldName) => {
+  const dateValue = get(rawObject, fieldName, null);
+
+  const utcDate = dayBeginInUTC(dateValue);
+
+  const normalizedDate = utcDate / 1000 || null;
+  return normalizedDate;
+};
+
 module.exports = {
   formateDate,
   fromNow,
   fromTo,
   addToDate,
   getWeek,
+  toEqualLocalTime,
+  dayBeginInUTC,
+  localDateFromApi,
+  localDateToApi,
 };
--- packages/utils/types/date.d.ts
+++ packages/utils/types/date.d.ts
@@ -10,4 +10,18 @@ declare module '@endpass/utils/date' {
   export function addToDate(date: DayJsDate, value: number, unit: string): Date;
 
   export function getWeek(date: Date): number;
+
+  export function toEqualLocalTime(date: DayJsDate): Date;
+
+  export function dayBeginInUTC(date: DayJsDate): number;
+
+  export function localDateFromApi(
+    object: object,
+    fieldName: string,
+  ): Date | null;
+
+  export function localDateToApi(
+    object: object,
+    fieldName: string,
+  ): number | null;
 }
