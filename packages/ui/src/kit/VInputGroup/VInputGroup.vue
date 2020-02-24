a0e39df9f03e89f6352548b00eecb664f89ef836
Danil Kosterin <raymondcostner@gmail.com>
2020-02-24 23:24:00 +0300

    feat(ui): add support for 'error' property in VInputGroup

--- packages/ui/src/kit/VInputGroup/VInputGroup.vue
+++ packages/ui/src/kit/VInputGroup/VInputGroup.vue
@@ -1,24 +1,47 @@
 <template>
-  <div :class="vInputGroupCssClass">
-    <slot />
+  <div>
+    <div :class="vInputGroupCssClass">
+      <slot />
+    </div>
+    <error-atom
+      v-if="isError"
+      :error="error"
+    />
   </div>
 </template>
 
 <script>
 import ThemeMixin from '@/mixins/ThemeMixin';
+import ErrorAtom from '@/atom/error-atom/error-atom';
 
 export default {
   name: 'VInputGroup',
+
+  props: {
+    error: {
+      type: String,
+      default: null,
+    },
+  },
 
   computed: {
     vInputGroupCssClass() {
       return {
         ...this.themeCssClass,
         'v-input-group': true,
+        'is-error': this.isError,
       };
+    },
+
+    isError() {
+      return !!this.error;
     },
   },
 
   mixins: [ThemeMixin],
+
+  components: {
+    ErrorAtom,
+  },
 };
 </script>
