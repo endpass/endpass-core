147cd0bdde4138de853f878e8c09061e44387b2e
idudiq <idudiq@gmail.com>
2020-03-16 11:37:52 +0300

    fix(ui): tabs content dublicates visible

--- packages/ui/src/kit/VTabs/VTabsContent.vue
+++ packages/ui/src/kit/VTabs/VTabsContent.vue
@@ -13,16 +13,24 @@ export default {
     if (activeTabIdx === null) return null;
 
     const tabs = $slots.default.filter($el => !!$el.tag);
-    const targetTab = tabs[activeTabIdx];
-
-    if (!targetTab) return null;
 
     return h(
       'section',
       {
         class: 'v-tabs-content',
       },
-      targetTab.componentOptions.children,
+      tabs.map((tabSlot, index) => h(
+        'div',
+        {
+          directives: [
+            {
+              name: 'show',
+              value: index === activeTabIdx,
+            },
+          ],
+        },
+        [tabSlot],
+      )),
     );
   },
 };
