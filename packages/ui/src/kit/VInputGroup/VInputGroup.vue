e436444af2273ade539ce7024075a9bea35224a4
lamartire <lamartire@gmail.com>
2020-03-20 18:09:40 +0300

    fix(ui): VInputGroup secondary skin

--- packages/ui/src/kit/VInputGroup/VInputGroup.vue
+++ packages/ui/src/kit/VInputGroup/VInputGroup.vue
@@ -10,11 +10,22 @@ import ThemeMixin from '@/mixins/ThemeMixin';
 export default {
   name: 'VInputGroup',
 
+  props: {
+    skin: {
+      type: String,
+      default: 'primary',
+      validator(value) {
+        return ['primary', 'secondary'].indexOf(value) !== -1;
+      },
+    },
+  },
+
   computed: {
     vInputGroupCssClass() {
       return {
         ...this.themeCssClass,
         'v-input-group': true,
+        [this.skin]: true,
       };
     },
   },
