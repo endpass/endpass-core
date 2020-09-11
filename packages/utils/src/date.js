1576e69e5a5e23af56cbed907b7c6acc2b9ba5f7
Danil Kosterin <raymondcostner@gmail.com>
2020-09-11 12:53:10 +0300

    feat(utils): typings for date utils

--- packages/utils/src/date.js
+++ packages/utils/src/date.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import weekOfYear from 'dayjs/plugin/weekOfYear';
@@ -8,23 +10,51 @@ import weekOfYear from 'dayjs/plugin/weekOfYear';
 dayjs.extend(relativeTime);
 dayjs.extend(weekOfYear);
 
+/**
+ * @param {dayjs.ConfigType} date
+ * @param {string} template
+ * @returns {string}
+ */
 const formateDate = (date, template = 'YYYY-MM-DD H:mm') =>
   dayjs(date).format(template);
 
+/**
+ * @param {dayjs.ConfigType} date
+ * @returns {string}
+ */
 const fromNow = date => dayjs(date).fromNow();
 
+/**
+ * @param {dayjs.ConfigType} fromDate
+ * @param {dayjs.ConfigType} toDate
+ * @returns {string}
+ */
 const fromTo = (fromDate, toDate) => {
   const start = dayjs(fromDate);
   return start.to(dayjs(toDate));
 };
 
+/**
+ * @param {dayjs.ConfigType} date
+ * @param {number} value
+ * @param {dayjs.OpUnitType} unit
+ * @returns {dayjs.ConfigType}
+ */
 const addToDate = (date, value, unit = 's') =>
   dayjs(date)
     .add(value, unit)
     .toDate();
 
+/**
+ * @param {dayjs.ConfigType} date
+ * @returns {number}
+ */
 const getWeek = date => dayjs(date).week();
 
+/**
+ * @param {Date | number | string} value
+ * @returns {Date}
+ */
 const toEqualLocalTime = value => {
   const date = new Date(value);
   const userTimezoneOffset = date.getTimezoneOffset() * 60000;
@@ -32,6 +62,10 @@ const toEqualLocalTime = value => {
   return normalizedDate;
 };
 
+/**
+ * @param {Date | number | string} value
+ * @returns {number}
+ */
 const toDayBeginInUTC = value => {
   const date = new Date(value);
 
@@ -47,12 +81,20 @@ const toDayBeginInUTC = value => {
   return utcDate;
 };
 
+/**
+ * @param {Date | number | string} day
+ * @returns {Date}
+ */
 const getStartDay = day => {
   const startDay = new Date(day);
   startDay.setHours(0, 0, 0, 0);
   return startDay;
 };
 
+/**
+ * @param {Date | number | string} day
+ * @returns {Date}
+ */
 const getEndDay = (day = Date.now()) => {
   const endDay = new Date(day);
   endDay.setHours(23, 59, 59, 999);
